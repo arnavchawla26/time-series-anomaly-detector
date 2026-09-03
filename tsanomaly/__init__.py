b4c428ace3342c1461fb75b96d8f5f794d6cd1f2
Arnav Chawla <71682840+arnavchawla26@users.noreply.github.com>
2026-09-03 06:23:20 -0700

    Scaffold project: packaging, license, gitignore, package init

---
+++ tsanomaly/__init__.py
@@ -0,0 +1,7 @@
+"""tsanomaly: dependency-free time series anomaly detection.
+
+Classical (moving-average) seasonal decomposition plus residual-based
+anomaly detectors (z-score, modified z-score / MAD, IQR, rolling z-score).
+"""
+
+__version__ = "0.1.0"
